6b7e913ac50f48183164118b46442767bceb9bdc
Oleksandr Yavorskyi <slimnation2@gmail.com>
2026-08-13 15:52:12 +0200

    future events - done

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const projectsSwiper = new Swiper('.projectsSlider', {
     prevEl: '.projects-prev',
   },
   breakpoints: {
-    320: { slidesPerView: 1, spaceBetween: 16 },
+    320: { slidesPerView: 1.2, spaceBetween: 16 },
     767: { slidesPerView: 2, spaceBetween: 24 }
   }
 });
